3f35f50088c1f6c0d36bca90d544d515267b0f9f
Julian Cole <juliancole@Julians-MacBook-Pro.local>
2020-08-23 15:21:55 -0700

    one endpoint working

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const server = require("./api/server.js");
 const accountRoutes = require('./routes/accountRoutes')
 const port = process.env.PORT || 8000
-
-
-
 server.use("/accounts", accountRoutes)
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,101 +1,104 @@
 const express = require('express');
 const db = require('../data/dbConfig')
-
 const router = express.Router();
 
-// Grabs all accounts
 
-
-
-router.get("/", async (req, res, next) => {
+router.get("/test", async (req, res, next) => {
     try {
      const accounts = await db.select("*").from("accounts")
-    
      res.json(accounts)
     } catch (err) {
         next(err)
     }
-
 })
-
-// Read account by ID 
-
+// Read account by ID
 router.get("/:id", async (req, res, next) => {
     try {
-        const [accounts] = await db
+        const account = await db
         .select("*")
         .from("accounts")
         .where("id", req.params.id)
         .limit(1)
-
         res.json(accounts)
     } catch (err) {
         next(err)
     }
 })
+// Create new account
+// router.post("/", async (req, res, next) => {
+//     try {
+//         const [id] = await db
+//         .insert ({
+//             name: req.body.name,
+//             budget: req.body.budget
+//         })
+//         .into("accounts")
+//
+//         const account = await db("accounts")
+//             // .select("*")
+//             // .from("accounts")
+//             .where("id", id)
+//             // .limit(1)
+//             .first()
+//         res.status(201).json(account)
+//     } catch (err){
+//         next(err)
+//     }
+// })
+// Update Account
+// router.post("/", async (req, res, next) => {
+//     try {
+//         const [id] = await db
+//         .insert ({
+//             name: req.body.name,
+//             budget: req.body.budget
+//         })
+//         .into("accounts")
+//
+//         const account = await db("accounts")
+//             // .select("*")
+//             // .from("accounts")
+//             .where("id", id)
+//             // .limit(1)
+//             .first()
+//         res.status(201).json(account)
+//     } catch (err){
+//         next(err)
+//     }
+// })
+// Delete Account
+// router.delete("/:id", async (req, res, next) => {
+//     try {
+//         await db("accounts")
+//         .where("id", req.params.id)
+//         .delete()
+//
+//         res.status(200).json({account: "your account has been deleted"})
+//     }   catch(err){
+//         next(err)
+//     }
+// })
 
 
-// Create new account 
 
+// // Create new account
+// router.post("/new", async (req, res, next) => {
+//     try {
+//         const accountId = await db
+//         .insert ({
+//             name: req.body.name || req.query.name,
+//             budget: req.body.budget || req.query.budget
+//         })
+//         .into("accounts")
+//         const account = await db
+//         .select("*")
+//         .from("accounts")
+//         .where("id", accountId)
+//         .limit(1)
+//         res.json(account)
+//     } catch (err){
+//         next(err)
+//     }
+// })
 
-
-router.post("/", async (req, res, next) => {
-    try {
-        const [id] = await db
-        .insert ({
-            name: req.body.name,
-            budget: req.body.budget
-        })
-        .into("accounts")
-
-        const account = await db("accounts")
-            // .select("*")
-            // .from("accounts")
-            .where("id", id)
-            // .limit(1)
-            .first()  
-        res.status(201).json(account)
-    } catch (err){
-        next(err)
-    }
-})
-
-// Update Account 
-
-router.post("/", async (req, res, next) => {
-    try {
-        const [id] = await db
-        .insert ({
-            name: req.body.name,
-            budget: req.body.budget
-        })
-        .into("accounts")
-
-        const account = await db("accounts")
-            // .select("*")
-            // .from("accounts")
-            .where("id", id)
-            // .limit(1)
-            .first()  
-        res.status(201).json(account)
-    } catch (err){
-        next(err)
-    }
-})
-
-// Delete Account 
-
-router.delete("/:id", async (req, res, next) => {
-    try {
-        await db("accounts")
-        .where("id", req.params.id)
-        .delete()
-
-        res.status(200).json({account: "your account has been deleted"})
-    }catch(err){
-        next(err)
-    }
-})
-
-
-module.exports = router 
+module.exports = router
