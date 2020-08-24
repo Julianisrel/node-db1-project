971f5e88ba0cd8a222c5d1af6e7a4d2ff2b5592b
Julian Cole <juliancole@Julians-MacBook-Pro.local>
2020-08-23 17:46:56 -0700

    completed

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -19,86 +19,74 @@ router.get("/:id", async (req, res, next) => {
         .from("accounts")
         .where("id", req.params.id)
         .limit(1)
-        res.json(accounts)
+        res.json(account)
     } catch (err) {
         next(err)
     }
 })
 // Create new account
-// router.post("/", async (req, res, next) => {
-//     try {
-//         const [id] = await db
-//         .insert ({
-//             name: req.body.name,
-//             budget: req.body.budget
-//         })
-//         .into("accounts")
-//
-//         const account = await db("accounts")
-//             // .select("*")
-//             // .from("accounts")
-//             .where("id", id)
-//             // .limit(1)
-//             .first()
-//         res.status(201).json(account)
-//     } catch (err){
-//         next(err)
-//     }
-// })
+router.post("/new", async (req, res, next) => {
+    try {
+        const id = await db
+        .insert ({
+            name: req.body.name,
+            budget: req.body.budget
+        })
+        .into("accounts")
+
+        const account = await db("accounts")
+            // .select("*")
+            // .from("accounts")
+            .where("id", id)
+            // .limit(1)
+            .first()
+        res.status(200).json(account)
+    } catch (err){
+        next(err)
+    }
+})
 // Update Account
-// router.post("/", async (req, res, next) => {
-//     try {
-//         const [id] = await db
-//         .insert ({
-//             name: req.body.name,
-//             budget: req.body.budget
-//         })
-//         .into("accounts")
-//
-//         const account = await db("accounts")
-//             // .select("*")
-//             // .from("accounts")
-//             .where("id", id)
-//             // .limit(1)
-//             .first()
-//         res.status(201).json(account)
-//     } catch (err){
-//         next(err)
-//     }
-// })
+router.post("/:id/update", async (req, res, next) => {
+    console.log('body: ', req)
+    try {
+        // const [id] = await db
+        // .insert ({
+        //     name: req.body.name,
+        //     budget: req.body.budget
+        // })
+        // .into("accounts")
+        console.log('re ', req)
+        await db("accounts")
+            .select("*")
+            .from("accounts")
+            .where("id", req.params.id)
+            .update({budget: req.body.budget, name: req.body.name})
+
+        const account = await db("accounts")
+        .select("*")
+        .from("accounts")
+        .where("id", req.params.id)
+        .first()
+        res.status(200).json(account)
+    } catch (err){
+        next(err)
+   }
+ })
 // Delete Account
-// router.delete("/:id", async (req, res, next) => {
-//     try {
-//         await db("accounts")
-//         .where("id", req.params.id)
-//         .delete()
-//
-//         res.status(200).json({account: "your account has been deleted"})
-//     }   catch(err){
-//         next(err)
-//     }
-// })
+router.delete("/:id/delete", async (req, res, next) => {
+    try {
+        await db("accounts")
+        .where("id", req.params.id)
+        .delete()
+
+        res.status(200).json({account: "your account has been deleted"})
+    }   catch(err){
+        next(err)
+    }
+})
 
 
 
-// // Create new account
-// router.post("/new", async (req, res, next) => {
-//     try {
-//         const accountId = await db
-//         .insert ({
-//             name: req.body.name || req.query.name,
-//             budget: req.body.budget || req.query.budget
-//         })
-//         .into("accounts")
-//         const account = await db
-//         .select("*")
-//         .from("accounts")
-//         .where("id", accountId)
-//         .limit(1)
-//         res.json(account)
-//     } catch (err){
-//         next(err)
-//     }
-// })
+
 
 module.exports = router
